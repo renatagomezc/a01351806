0993080300c4db3a6f0d2a8724530f4db5c92a66
renatagomezc <131408628+renatagomezc@users.noreply.github.com>
2023-06-05 19:17:38 -0600

    Update actividad_integradora_uf6.py

--- actividad_integradora_uf6.py
+++ actividad_integradora_uf6.py
@@ -17,8 +17,11 @@ df = pd.read_csv('https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
+Latitude = df['Latitude']
+Longitude = df['Longitude']
+
 mapa=pd.DataFrame(
-    np.array([[latitude, longitude]]),
+    np.array([[Latitude, Longitude]]),
     columns=['lat', 'lon'])
 
 mapa = mapa.dropna()
