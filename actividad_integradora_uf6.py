01134fb8149025d27e191a4e89c219eefde24040
renatagomezc <131408628+renatagomezc@users.noreply.github.com>
2023-06-05 19:43:40 -0600

    Update actividad_integradora_uf6.py

--- actividad_integradora_uf6.py
+++ actividad_integradora_uf6.py
@@ -13,7 +13,7 @@ import numpy as np
 
 st.title('Police Incident Reports form 1028 to 2020 in San Francisco')
 
-df = pd.read_csv('https://drive.google.com/file/d/1thrhsmjiTQ1U5ZF6fsXbT-OW4wH_BvTa/view?usp=drive_link')
+df = pd.read_csv('https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=drive_link')
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
