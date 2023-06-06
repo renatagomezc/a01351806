085fda5714d10422f600cc14342a932c7c0ae2f8
renatagomezc <131408628+renatagomezc@users.noreply.github.com>
2023-06-05 19:04:23 -0600

    Update actividad_integradora_uf6.py

--- actividad_integradora_uf6.py
+++ actividad_integradora_uf6.py
@@ -9,6 +9,7 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title('Police Incident Reports form 1028 to 2020 in San Francisco')
 
@@ -16,7 +17,9 @@ df = pd.read_csv('https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
-mapa=pd.DataFrame()
+mapa=pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
 mapa = mapa.dropna()
 st.map(mapa.astype(int))
