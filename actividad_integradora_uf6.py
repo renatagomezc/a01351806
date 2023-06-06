ad1d15ccc15fffa4ebd2cc6dc4dbf699bb3c739a
renatagomezc <131408628+renatagomezc@users.noreply.github.com>
2023-06-05 19:16:53 -0600

    Update actividad_integradora_uf6.py

--- actividad_integradora_uf6.py
+++ actividad_integradora_uf6.py
@@ -18,8 +18,8 @@ df = pd.read_csv('https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
 mapa=pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+    np.array([[latitude, longitude]]),
+    columns=['lat', 'lon'])
 
 mapa = mapa.dropna()
 st.map(mapa.astype(float))
